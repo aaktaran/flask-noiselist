d16aae2aea148b3b99203934fcedd85056983e9b
Anil Aktaran <aaktaran@linkedin.com>
2012-09-12 23:23:38 -0700

    updated to work on windows

--- src/noiselist/model.py
+++ src/noiselist/model.py
@@ -1,9 +1,9 @@
 from flask import Flask
-from flaskext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
 
--- src/noiselist/script.py
+++ src/noiselist/script.py
@@ -14,25 +14,30 @@ DEPLOY_CFG = etc('deploy.cfg')
 
 DEBUG_INI = etc('debug.ini')
 DEBUG_CFG = etc('debug.cfg')
-
+ 
 _buildout_path = __file__
+print _buildout_path, "test"
 for i in range(2 + __name__.count('.')):
     _buildout_path = os.path.dirname(_buildout_path)
 
 abspath = partial(os.path.join, _buildout_path)
+print os.path.join, _buildout_path
 del _buildout_path
 
 
-# bin/paster serve parts/etc/deploy.ini
+# bin\paster serve parts/etc/deploy.ini
 def make_app(global_conf={}, config=DEPLOY_CFG, debug=False):
     from noiselist import app
+    
     app.config.from_pyfile(abspath(config))
     app.debug = debug
+    
     return app
 
 
 # bin/paster serve parts/etc/debug.ini
 def make_debug(global_conf={}, **conf):
+    
     from werkzeug.debug import DebuggedApplication
     app = make_app(global_conf, config=DEBUG_CFG, debug=True)
     return DebuggedApplication(app, evalex=True)
@@ -72,7 +77,7 @@ def _serve(action, debug=False, dry_run=False):
         config = DEBUG_INI
     else:
         config = DEPLOY_INI
-    argv = ['bin/paster', 'serve', config]
+    argv = ['bin/paster-script.py', 'serve', config]
     if action in ('start', 'restart'):
         argv += [action, '--daemon']
     elif action in ('', 'fg', 'foreground'):
@@ -97,7 +102,7 @@ def _serve(action, debug=False, dry_run=False):
 # bin/flask-ctl ...
 def run():
     action_shell = werkzeug.script.make_shell(make_shell, make_shell.__doc__)
-
+    
     # bin/flask-ctl serve [fg|start|stop|restart|status|initdb]
     def action_serve(action=('a', 'start'), dry_run=False):
         """Serve the application.
@@ -110,20 +115,23 @@ def run():
          - '--dry-run' print the paster command and exit
         """
         _serve(action, debug=False, dry_run=dry_run)
-
+   
+    
     # bin/flask-ctl debug [fg|start|stop|restart|status|initdb]
     def action_debug(action=('a', 'start'), dry_run=False):
         """Serve the debugging application."""
         _serve(action, debug=True, dry_run=dry_run)
-
+    
     # bin/flask-ctl status
     def action_status(dry_run=False):
         """Status of the application."""
         _serve('status', dry_run=dry_run)
-
+    
     # bin/flask-ctl stop
     def action_stop(dry_run=False):
         """Stop the application."""
         _serve('stop', dry_run=dry_run)
+    print "TEST3"
+    print make_shell.__doc__
     werkzeug.script.run()
 
